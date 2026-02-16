01026f1b435cc4b3e5a19d480949c8949db081e2
Pratik Devalatkar <pratikdevalatkar45@gmail.com>
2026-02-16 17:41:29 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Parts Inventory API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
