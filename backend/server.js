70e657f7969709e6bb828b2ece3d41925a049ad4
Pratik Devalatkar <pratikdevalatkar45@gmail.com>
2026-02-16 17:50:49 +0530

    mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Parts Inventory API Running...");
 });
+//mongoose connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
