461c9fd47a3c927b117cbcaea86af58dc328d0e3
Pratik Devalatkar <pratikdevalatkar45@gmail.com>
2026-02-17 11:40:39 +0530

    feat: add production environment configuration

--- frontend/src/app/services/part.ts
+++ frontend/src/app/services/part.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartService {
 
-  private apiUrl = 'http://localhost:5000/api/parts';
+  private apiUrl = `${environment.apiUrl}/parts`;;
 
   constructor(private http: HttpClient) {}
 
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://spare-part-qxzo.onrender.com/api'
+};
